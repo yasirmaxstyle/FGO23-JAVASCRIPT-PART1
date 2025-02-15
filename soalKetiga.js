ebd5822151f00c6e3cab356fe9d9e683e722f668
yasirmaxstyle <yasirmu77@gmail.com>
2025-02-15 14:52:58 +0700

    revisiSoalKetiga

--- soalKetiga.js
+++ soalKetiga.js
@@ -1,16 +1,53 @@
-// celcius = 0 - 100
-// fahrenheit = 32 - 212
-// reamur = 0 - 80
-// kelvin = 273 - 373
-function getRandom(min, max) {
-    return Math.floor(Math.random() * max - min + min);
+function tempConvertor(temp, from, to) {
+    // invalid type
+    if (typeof temp !== 'number') return;
+    if (typeof from !== 'string') return;
+    if (typeof to !== 'string') return;
+
+    const tempArr = ['C', 'R', 'F', 'K'];
+    // invalid case    
+    if (from === to) {
+        console.log('You cannot convert to the same temperature')
+        return;
+    } else if (!tempArr.includes(from)) {
+        console.log('Please input: C, F, R, or K');
+        return;
+    } else if (!tempArr.includes(to)) {
+        console.log('Please input: C, F, R, or K');
+        return;
+    }
+
+    // define value temp
+    let vFrom;
+    let vTo;
+
+    switch (from) {
+        case 'C': vFrom = 5; break;
+        case 'F': vFrom = 9; break;
+        case 'R': vFrom = 4; break;
+        case 'K': vFrom = 5; break;
+    }
+
+    switch (to) {
+        case 'C': vTo = 5; break;
+        case 'F': vTo = 9; break;
+        case 'R': vTo = 4; break;
+        case 'K': vTo = 5; break;
+    }
+
+    switch (from) {
+        case 'F': temp -= 32; break;
+        case 'K': temp -= 273; break;
+    }
+
+    let result = vTo / vFrom * temp; // counting result
+
+    switch (to) {
+        case 'F': result += 32; break;
+        case 'K': result += 273; break;
+    }
+
+    console.log(result);
 }
 
-let celcius = getRandom(0, 100);
-let fahrenheit = getRandom(32, 212);
-let reamur = getRandom(0, 80);
-let kelvin = getRandom(273, 373);
-
-function konveriSuhu(suhuAwal, suhuAkhir) {
-    if 
-}
+tempConvertor(100, 'K', 'C');
